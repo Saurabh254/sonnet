84cc70a86268b66e716e87428c25da13d0b71b3e
Saurabh <sauravvishwakarma030@gmail.com>
2024-10-13 01:52:19 +0530

    feat(exception handling) : added base exception handling class for handling and serializing exception

---
+++ backend/app/exceptions.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import Literal
+from pydantic import create_model
+
+
+class ServerError(Exception):
+    def __init__(
+        self,
+        status_code: int = 500,
+        message: str = "unexpected error occurred",
+        headers: dict[str, str] | None = {},
+    ) -> None:
+        super().__init__(message)
+        self.message = message
+        self.status_code = status_code
+        self.headers = headers
+
+    @classmethod
+    def schema(cls) -> type[BaseModel]:
+        type_literal = Literal[cls.__name__]  # type: ignore
+
+        return create_model(cls.__name__, type=(type_literal, ...), detail=(str, ...))
+
+
+class ResourceNotFound(ServerError):
+    def __init__(self, message: str = "Not found", status_code: int = 404) -> None:
+        super().__init__(message=message, status_code=status_code)
---
+++ backend/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from app.exception_handling import add_exception_handler
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    title="Sonnet",
+    description="A FastAPI application build for a purpose.",
+    version="0.0.1",
+    contact={
+        "name": "Saurabh Vishwakarma",
+        "url": "https://www.saurabhvishwakarma.in",
+        "email": "contact@saurabhvishwakarma.in",
+    },
+    license_info={
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+)
+
+
+add_exception_handler(app)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get(
+    "/health",
+    tags=["internal"],
+    summary="Health Check",
+    description="This endpoint checks the health status of the application and returns a message indicating if the service is operational."
+)
+async def check_health():
+    return {"status": "we're up"}
